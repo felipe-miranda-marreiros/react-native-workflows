525a7d7f600a6195054040808455876b0cf1cbb1
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-03-23 11:20:58 -0300

    feat: add github workspace to decrypt script

--- .github/scripts/decrypt_android_keys.sh
+++ .github/scripts/decrypt_android_keys.sh
@@ -1,8 +1,8 @@
 echo "$KEYSTORE" > keystore.asc 
-gpg -d --passphrase="$ANDROID_KEYSTORE_PASSPHRASE" --batch keystore.asc  > android/fastlane/keystore.jks
+gpg -d --passphrase="$ANDROID_KEYSTORE_PASSPHRASE" --batch keystore.asc  > "$GITHUB_WORKSPACE/android/fastlane/keystore.jks"
 
 echo "$PLAYSTORE" > play-store-credentials.json.asc 
-gpg -d --passphrase="$ANDROID_PLAYSTORE_PASSPHRASE" --batch play-store-credentials.json.asc > android/play-store-credentials.json
+gpg -d --passphrase="$ANDROID_PLAYSTORE_PASSPHRASE" --batch play-store-credentials.json.asc > "$GITHUB_WORKSPACE/android/play-store-credentials.json"
 
 rm keystore.asc
 rm play-store-credentials.json.asc
